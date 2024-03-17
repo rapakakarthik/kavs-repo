8ef98a3cb9fdf689e0a24397dbc56ac8b3a77e4a
rapakakarthik <rapakakarthik7@gmail.com>
2024-03-17 19:07:28 +0530

    this file is modified and committed into subnets 1 and 2

--- main.tf
+++ main.tf
@@ -17,3 +17,21 @@ resource "aws_internet_gateway" "dev" {
     }
   
 }
+
+resource "aws_subnet" "dev" {
+    vpc_id = aws_vpc.dev.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+      Name = "kavya_subnet1"
+
+      vpc_id = aws_vpc.dev.id
+      cidr_block = "10.0.2.0/24"
+      availability_zone = "us-east-1b"
+
+      tags = {
+        Name = "kavya_subnet2"
+      }
+    }
+}
