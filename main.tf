1047953ea5c6ef8040133f7b83a1cabd2596232e
rapakakarthik <rapakakarthik7@gmail.com>
2024-03-17 19:13:18 +0530

    this file is committed on subnets as map lauunch true

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "aws_internet_gateway" "dev" {
 resource "aws_subnet" "dev" {
     vpc_id = aws_vpc.dev.id
     cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
     availability_zone = "us-east-1a"
 
     tags = {
@@ -28,10 +29,21 @@ resource "aws_subnet" "dev" {
 
       vpc_id = aws_vpc.dev.id
       cidr_block = "10.0.2.0/24"
+      map_public_ip_on_launch = true
       availability_zone = "us-east-1b"
+      
 
       tags = {
         Name = "kavya_subnet2"
       }
     }
+}
+
+resource "aws_route_table" "dev" {
+ vpc_id = aws_vpc.dev.id
+
+ tags = {
+   Name = "kavya_rt"
+ }
+
 }
